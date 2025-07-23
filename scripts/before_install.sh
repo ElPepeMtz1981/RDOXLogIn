72e3b146f5c29a44e243c4f5cd09f8f95cf347bd
ElPepeMtz1981 <elpepemtz@gmail.com>
2025-07-23 11:25:52 -0600

    tri ci cd 11:25 am

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,3 +1,5 @@
 ﻿#!/bin/bash
+echo "Start before_install.sh" >> /home/ubuntu/rdoxlogin.log
 mkdir -p /home/ubuntu/rdoxlogin
 mkdir -p /home/ubuntu/rdoxloginsc
+echo "end before_install.sh" >> /home/ubuntu/rdoxlogin.log
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,9 +1,9 @@
 ﻿#!/bin/bash
 set -e
 
-echo "🚀 Ejecutando start_server.sh..."
+echo "Ejecutando start_server.sh..."
 
 # Restart the api service
 sudo systemctl restart LogInService
 
-echo "✅ Servicio reiniciado correctamente."
+echo "Servicio reiniciado correctamente."
