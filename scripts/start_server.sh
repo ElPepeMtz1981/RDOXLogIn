1d15b829221a307dd2caa05956736073a39dbe1d
ElPepeMtz1981 <elpepemtz@gmail.com>
2025-07-23 14:39:39 -0600

    try ci/cd

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,9 +1,9 @@
 ﻿#!/bin/bash
 set -e
 
-echo "Ejecutando start_server.sh..."
+echo "Ejecutando start_server.sh..." >> /home/ubuntu/rdoxlogin.log
 
 # Restart the api service
 sudo systemctl restart LogInService
 
-echo "Servicio reiniciado correctamente."
+echo "Servicio reiniciado correctamente." >> /home/ubuntu/rdoxlogin.log
